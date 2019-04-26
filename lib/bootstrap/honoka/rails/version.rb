aa73add8e5be8f6e6cd994626dfb205f88996b4b
takmg <takmg@users.noreply.github.com>
2019-04-27 00:02:01 +0900

    テストコードとVersionの更新

--- lib/bootstrap/honoka/rails/version.rb
+++ lib/bootstrap/honoka/rails/version.rb
@@ -1,10 +1,11 @@
 module Bootstrap
   module Honoka
     module Rails
-      VERSION = '4.3.1'.freeze
+      VERSION = '3.3.7'.freeze
       def self.major_ver
         VERSION.split('.').first.to_i
       end
+
       def self.dependency_ver
         VERSION.split('.')[0..2].join('.')
       end
