dd8cea9e9b5e4b23431f47ee35cd1dacbcfc0d6f
takmg <takmg@users.noreply.github.com>
2019-05-22 11:41:09 +0900

    4.3.1のテストコードを更新

--- test/honoka_test.rb
+++ test/honoka_test.rb
@@ -1,10 +1,25 @@
 require 'test_helper'
 
 class Bootstrap::Honoka::Rails::Test < ActionDispatch::IntegrationTest
-  %w[honoka].each do |name|
-    test "access #{name} test" do
-      get "/pages/#{name}"
+
+  # Mix-in
+  include DummyIntegration
+
+  # constant variables
+  TEST_PAGES = %w[honoka]
+  TEST_PATHS = TEST_PAGES.map { |el| "/pages/#{el}" }
+  TEST_DATAS = TEST_PAGES.zip( TEST_PATHS )
+
+  # アクセステスト
+  TEST_DATAS.each do |page,path|
+    test "access #{page}" do
+      # ページの取得確認
+      get path
       assert_response :success
+
+      # screenshot!
+      visit path
+      take_full_page_screenshot!
     end
   end
 end
