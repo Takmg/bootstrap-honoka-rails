e0faa4070ead2c633a0f934f210bb6e6e95a897f
takmg <takmg@users.noreply.github.com>
2019-05-24 01:19:56 +0900

    テストコードの修正

--- test/honoka_test.rb
+++ test/honoka_test.rb
@@ -1,25 +1,29 @@
 require 'test_helper'
 
 class Bootstrap::Honoka::Rails::Test < ActionDispatch::IntegrationTest
-
   # Mix-in
   include DummyIntegration
 
   # constant variables
-  TEST_PAGES = %w[honoka umi nico]
-  TEST_PATHS = TEST_PAGES.map { |el| "/pages/#{el}" }
-  TEST_DATAS = TEST_PAGES.zip( TEST_PATHS )
+  TEST_PAGES = %w[honoka umi nico].map(&:freeze).freeze
+  TEST_PATHS = TEST_PAGES.map { |el| "/pages/#{el}".freeze }.freeze
+  TEST_DATAS = TEST_PAGES.zip(TEST_PATHS).map(&:freeze).freeze
 
   # アクセステスト
-  TEST_DATAS.each do |page,path|
+  TEST_DATAS.each do |page, path|
     test "access #{page}" do
-      # ページの取得確認
-      get path
-      assert_response :success
+      begin
+        # ページのアクセステスト
+        visit path
 
-      # screenshot!
-      visit path
-      take_full_page_screenshot!
+        # ステータスコードの取得を行う
+        # https://stackoverflow.com/questions/7908907/how-to-test-the-response-code-with-capybara-selenium
+        response_code = Capybara.page.first('html')[:code].to_i
+        assert response_code == 200, 'エラー：正しくページを取得できませんでした。'
+      ensure
+        # エラーであっても表示ページのscreenshotは撮る。
+        take_full_page_screenshot!
+      end
     end
   end
 end
