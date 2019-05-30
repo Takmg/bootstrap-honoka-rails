7b6a4daa7417282dd519e70ecc88fffc497c6442
takmg <takmg@users.noreply.github.com>
2019-05-30 15:21:08 +0900

    stylesheetsを定数化 及び それに伴う修正

--- lib/bootstrap/honoka/rails.rb
+++ lib/bootstrap/honoka/rails.rb
@@ -3,12 +3,11 @@ require 'bootstrap/honoka/rails/engine'
 module Bootstrap
   module Honoka
     module Rails
-      class << self
-        def stylesheets
-          Dir.glob("#{File.join(File.expand_path('../../..', __dir__), 'assets', 'stylesheets')}/*.scss")
-             .map { |f| f.match(%r{.+/_([^/]+?)\.scss})[1] }
-        end
-      end
+      # サポートしているスタイルシート一覧
+      STYLE_SHEETS =
+        Dir.glob("#{File.join(File.expand_path('../../..', __dir__), 'assets', 'stylesheets')}/*.scss")
+           .map { |f| f.match(%r{.+/_([^/]+?)\.scss})[1].to_sym.freeze }
+           .freeze
     end
   end
 end
